e77daca0ebe827614a162c32514f2e2bd22f207d
banana-stellare <aless.benni@gmail.com>
2026-01-02 16:37:29 +0000

    Progetto Liste Completo direi

--- Liste3.0/src/App.java
+++ Liste3.0/src/App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) {
+
+        // crea una lista di stringhe con dati di prova, la manipola con i metodi presenti ed infine la stampa
+        Lista l = new Lista();
+	    l.add( "Mario" );
+	    l.addHead(new Nodo("Prima di Mario"));
+	    l.addTail(new Nodo("Dopo di Mario"));
+        
+		System.out.println("Lunghezza lista: " + l.getLength());
+		System.out.println("Contiene 'Mario'? " + l.exists("Mario"));
+		System.out.println("Contiene 'Luigi'? " + l.exists("Luigi"));
+		System.out.println(l);
+		System.out.println("Rimuovo 'Mario': " + l.remove("Mario"));
+		System.out.println(l);
+	}
 }
