80728433c5721d4620661134342cc9f96e30ed3e
banana-stellare <aless.benni@gmail.com>
2026-01-26 13:53:16 +0000

    Quasi finito, manca la rimozione da file

--- Esercizio_24a/src/Porto.java
+++ Esercizio_24a/src/Porto.java
@@ -1,27 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Porto {
     private Barca[] PORTO;
+    File f;
+    PrintWriter pw;
 
     public Porto() {
         PORTO = new Barca[100];
+        f = new File("Data/porto.txt");
+        try {
+            pw = new PrintWriter(f);
+        } catch (FileNotFoundException e) {
+            System.out.println("Errore nella creazione del file");
+        } catch (Exception e) {
+            System.out.println("Errore");
+        }
     }
 
-    public boolean parkBarca(Barca b1){
-        if(b1.getTb()== TipoBarca.VELA){
-            for (int i=50; i< PORTO.length;i++){
-                if( PORTO[i]==null){
-                    PORTO[i]= b1;
+    public boolean parkBarca(Barca b1) {
+        if (b1.getTb() == TipoBarca.VELA) {
+            for (int i = 50; i < PORTO.length; i++) {
+                if (PORTO[i] == null) {
+                    PORTO[i] = b1;
+                    pw.println("Posizione barca: " + i + " " + b1.toString());
+                    pw.close();
                     return true;
                 }
             }
             return false;
         }
-
-        int inizio=(b1.getLunghezza()< 10 ? 0 : 21);
-        for (int i=inizio; i<PORTO.length; i++){
-             if( PORTO[i]==null){
-                    PORTO[i]= b1;
-                    return true;
-                }
+        int inizio = (b1.getLunghezza() < 10 ? 0 : 21);
+        for (int i = inizio; i < PORTO.length; i++) {
+            if (PORTO[i] == null) {
+                PORTO[i] = b1;
+                pw.println("Posizione barca: " + i + " " + b1.toString());
+                pw.close();
+                return true;
+            }
         }
         return false;
 
@@ -38,24 +55,60 @@ public class Porto {
         return;
     }
 
-    public String toStrin(){
-        String s="";
-        for (int i=0; i< PORTO.length;i++){
-            if(PORTO[i]!=null){
-                s+= "Posizione "+ i + ": " + PORTO[i].getNome() + "\n";
+    public void checkPosizione(int posizione) {
+        if (PORTO[posizione] != null) {
+            System.out.println("La barca nella posizione numero " + posizione + " è: " + PORTO[posizione]);
+        } else {
+            System.out.println("La posizione è vuota");
+        }
+    }
+
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < PORTO.length; i++) {
+            if (PORTO[i] != null) {
+                s += "Posizione " + i + " " + PORTO[i] + "\n";
             }
         }
         return s;
     }
-    public String toString(){
-        String s="";
-        for (int i=0; i< PORTO.length;i++){
-            if(PORTO[i]!=null){
-                s+= "Posizione "+ i + " " + PORTO[i] + "\n";
+
+    public double affitto(int giorni, Barca b1) {
+        double affitto = 0;
+        if (b1.getTb() == TipoBarca.VELA) {
+            affitto = b1.getLunghezza() * 10 * giorni;
+            return affitto;
+        } else if (b1.getTb() == TipoBarca.MOTORE) {
+            affitto = b1.getStazza() * 20 * giorni;
+            return affitto;
+        }
+
+        return 0;
+
+    }
+
+    public void removeBarca(String nome) {
+        for (int i = 0; i < PORTO.length; i++) {
+            if (PORTO[i] != null && (PORTO[i].getNome()).equalsIgnoreCase(nome)) {
+                PORTO[i] = null;
+                return;
             }
         }
-        return s;
+    }
+
+    public void removeBarca(Barca b1) {
+        for (int i = 0; i < PORTO.length; i++) {
+            if (PORTO[i] == b1) {
+                PORTO[i] = null;
+                return;
+            }
+        }
+    }
+
+    public void removeBarca(int posizione) {
+        if (PORTO[posizione] != null) {
+            PORTO[posizione] = null;
+        }
     }
 
 }
-
